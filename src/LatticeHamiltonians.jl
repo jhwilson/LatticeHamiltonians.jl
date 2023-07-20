34bab8281f7a1ddbe36036767ee16b3a80b93607
Noah Rahman <selimthegrim@gmail.com>
2023-07-20 09:46:37 -0700

    cleanup
    added missing parenthesis in expr

--- src/LatticeHamiltonians.jl
+++ src/LatticeHamiltonians.jl
@@ -89,7 +89,7 @@ function loop_periodic(s, hop, ex, j, flag)
         for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
             $(loop_periodic(s, hop, ex, j - 1))
         end;
-        ind2 += d * cumprod(N[1:j], 1);
+        ind2 += d * cumprod(N[1:j], 1));
     elseif m > 0
         if (flag)
             expr = :(for $(Symbol("$(s)$j")) = 1:(N[$j]-$m)
