2b5b1f503e55da23ed15f34b62f88db6e3cb9105
Noah Rahman <selimthegrim@gmail.com>
2023-07-20 12:55:56 -0700

    split exps correctly
    probably less verbose way to do this by concatenating quote blocks
    
    got rid of unnecessary ends

--- src/LatticeHamiltonians.jl
+++ src/LatticeHamiltonians.jl
@@ -83,26 +83,39 @@ function loop_periodic(s, hop, ex, j, flag)
         if (flag)
             expr = :(for $sj = 1:$(-m)
                 $(loop_periodic(s, hop, ex, j - 1))
-            end)
+            end;
+            ind2 -= d * cumprod(N[1:j], 1); #NB: cumprod and prod should both work left to right, also use Arrayview here? subarray?
+            for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
+                $(loop_periodic(s, hop, ex, j - 1))
+            end;
+            ind2 += d * cumprod(N[1:j], 1))
+        else
             expr = :(
+
+            ind2 -= d * cumprod(N[1:j], 1); #NB: cumprod and prod should both work left to right, also use Arrayview here? subarray?
+            for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
+                $(loop_periodic(s, hop, ex, j - 1))
+            end;
+            ind2 += d * cumprod(N[1:j], 1))
         end
-        ind2 -= d * cumprod(N[1:j], 1); #NB: cumprod and prod should both work left to right, also use Arrayview here? subarray?
-        for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
-            $(loop_periodic(s, hop, ex, j - 1))
-        end;
-        ind2 += d * cumprod(N[1:j], 1))
     elseif m > 0
         if (flag)
             expr = :(for $(Symbol("$(s)$j")) = 1:(N[$j]-$m)
                 $(loop_periodic(s, hop, ex, j - 1))
-            end;)
+            end;
+            ind2 -= d * cumprod(N[1:j], 1);
+            for $(Symbol("$(s)$j")) = (N[$j]-$(m - 1)):N[$j]
+                $(loop_periodic(s, hop, ex, j - 1))
+            end;
+            ind2 += d * cumprod(N[1:j], 1))
+        else
             expr = :(
+            ind2 -= d * cumprod(N[1:j], 1);
+            for $(Symbol("$(s)$j")) = (N[$j]-$(m - 1)):N[$j]
+                $(loop_periodic(s, hop, ex, j - 1))
+            end;
+            ind2 += d * cumprod(N[1:j], 1))
         end
-        ind2 -= d * cumprod(N[1:j], 1);
-        for $(Symbol("$(s)$j")) = (N[$j]-$(m - 1)):N[$j]
-            $(loop_periodic(s, hop, ex, j - 1))
-        end;
-        ind2 += d * cumprod(N[1:j], 1))
     else
         expr = :(for $(Symbol("$(s)$j")) = 1:N[$j]
             $(loop_periodic(s, hop, ex, j - 1))
