1ccca6fbd996c3d10b3797966e796791e7ad8949
Justin H. Wilson <justin@jhwilson.com>
2023-07-25 00:05:48 -0500

    Revert "adding open BCs"
    This reverts commit 8e181269642a9145bbc35c870207139e8d831361.

--- src/LatticeHamiltonians.jl
+++ src/LatticeHamiltonians.jl
@@ -73,7 +73,7 @@ loop_periodic_diag(dim, d, ex; s = :n) = :(ind1 = 0;
     $(loop_periodic(s, zeros(Int, dim), ex, dim)))
 
     
-function loop_periodic(s, hop, ex, j, flag)
+function loop_periodic(s, hop, ex, j)
     if j == 0
         return ex
     end
@@ -83,39 +83,25 @@ function loop_periodic(s, hop, ex, j, flag)
         if (flag)
             expr = :(for $sj = 1:$(-m)
                 $(loop_periodic(s, hop, ex, j - 1))
-            end;
-            ind2 -= d * cumprod(N[1:j], 1); #NB: cumprod and prod should both work left to right, also use Arrayview here? subarray?
-            for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
-            end;
-            ind2 += d * cumprod(N[1:j], 1))
-        else
-            expr = :(
-
-            ind2 -= d * cumprod(N[1:j], 1); #NB: cumprod and prod should both work left to right, also use Arrayview here? subarray?
-            for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
-            end;
-            ind2 += d * cumprod(N[1:j], 1))
-        end
+            end;)
+        expr = :(
+            inc = 
+        ind2 -= d * cumprod(N[1:j], 1); #NB: cumprod and prod should both work left to right, also use Arrayview here? subarray?
+        for $(Symbol("$(s)$j")) = $(-m + 1):N[$j]
+            $(loop_periodic(s, hop, ex, j - 1))
+        end;
+        ind2 += d * cumprod(N[1:j], 1);
     elseif m > 0
         if (flag)
             expr = :(for $(Symbol("$(s)$j")) = 1:(N[$j]-$m)
                 $(loop_periodic(s, hop, ex, j - 1))
-            end;
-            ind2 -= d * cumprod(N[1:j], 1);
-            for $(Symbol("$(s)$j")) = (N[$j]-$(m - 1)):N[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
-            end;
-            ind2 += d * cumprod(N[1:j], 1))
-        else
-            expr = :(
-            ind2 -= d * cumprod(N[1:j], 1);
-            for $(Symbol("$(s)$j")) = (N[$j]-$(m - 1)):N[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
-            end;
-            ind2 += d * cumprod(N[1:j], 1))
-        end
+            end;)
+        expr = :(
+        ind2 -= d * cumprod(N[1:j], 1);
+        for $(Symbol("$(s)$j")) = (N[$j]-$(m - 1)):N[$j]
+            $(loop_periodic(s, hop, ex, j - 1))
+        end;
+        ind2 += d * cumprod(N[1:j], 1))
     else
         expr = :(for $(Symbol("$(s)$j")) = 1:N[$j]
             $(loop_periodic(s, hop, ex, j - 1))
