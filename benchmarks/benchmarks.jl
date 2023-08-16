4616d7f56b8d1048490ebf2e26971b1495db4fd5
Zach Raines <dev@zmraines.com>
2023-08-16 15:01:08 -0500

    benchmark: add a simple benchmark

---
+++ benchmarks/benchmarks.jl
@@ -0,0 +1,38 @@
+using LatticeHamiltonians
+using BenchmarkTools
+using LinearAlgebra
+using Arpack
+
+# Define the Hamiltonian to benchmark
+BENCHMARK_SIZE = 200
+
+H = @lattice_hamiltonian begin
+    L = [100]
+    V = [0.0, 0.0]
+    (0) -> [0 t1; t1 0]
+    (1) -> [0 t2; 0 0]
+    (-1) -> [0 0; t2 0]
+    t1 = 1.0
+    t2 = 2.0
+end
+
+Hsparse = sparse(H)
+
+Hmat = diagm(
+    1 => [complex(1.0 + iseven(j)) for j = 1:(BENCHMARK_SIZE-1)],
+    -1 => [complex(1.0 + iseven(j)) for j = 1:(BENCHMARK_SIZE-1)],
+)
+Hmat[1, BENCHMARK_SIZE] = 2
+Hmat[BENCHMARK_SIZE, 1] = 2
+
+# Define a benchmark suite
+
+SUITE = BenchmarkGroup()
+
+Eigs = BenchmarkGroup()
+
+Eigs["Manual"] = @benchmarkable eigs($(Hmat))
+Eigs["Lattice Hamiltonians"] = @benchmarkable eigs($(H))
+Eigs["Sparse"] = @benchmarkable eigs($(Hsparse))
+
+SUITE["eigs"] = Eigs
