f5c6893341738e63dc04f2fb808730c4272b360e
Zach Raines <dev@zmraines.com>
2023-08-22 15:39:14 -0500

    test: add tests for nonzero_elements

---
+++ test/exprs.jl
@@ -0,0 +1,25 @@
+using LatticeHamiltonians: nonzero_elements
+
+@testset "Expression manipulation" begin
+    @testset "nonzero_elements" begin
+        @test nonzero_elements(:([1 0; 0 1])) ==
+              (rows = [1, 2], cols = [1, 2], vals = [1, 1])
+        @test nonzero_elements(:([x 0; 0 y])) ==
+              (rows = [1, 2], cols = [1, 2], vals = [:x, :y])
+        @test nonzero_elements(:([
+            1 0 2
+            3 4.0 0
+            ψ 0.0 ϕ
+        ])) == (
+            rows = [1, 1, 2, 2, 3, 3],
+            cols = [1, 3, 1, 2, 1, 3],
+            vals = [1, 2, 3, 4, :ψ, :ϕ],
+        )
+        # Test expressions with that clearly evaluate to zero
+        @test nonzero_elements(:([0im 0 + 0 0 - 0 0 * 0])) ==
+              (row = [], cols = [], vals = []) broken = true
+        # Test literal expressions or literal complex numbers
+        @test nonzero_elements(:([4im, 0, 1 + 2im, 2 * 3])) ==
+              (rows = [1, 2, 2], cols = [1, 1, 2], vals = [4im, 1 + 2im, 6]) broken = true
+    end
+end
