b60a65a891e0c96e5980239546376d9ca9273de2
Zach Raines <dev@zmraines.com>
2023-08-23 10:43:36 -0500

    test: fix test failures on julia 1.6

--- test/exprs.jl
+++ test/exprs.jl
@@ -22,11 +22,11 @@ using LatticeHamiltonians:
             vals = [1, 2, 3, 4, :ψ, :ϕ],
         )
         # Test expressions with that clearly evaluate to zero
-        @test nonzero_elements(:([0im 0 + 0 0 - 0 0 * 0])) ==
-              (row = [], cols = [], vals = []) broken = true
+        @test_broken nonzero_elements(:([0im 0 + 0 0 - 0 0 * 0])) ==
+                     (row = [], cols = [], vals = [])
         # Test literal expressions or literal complex numbers
-        @test nonzero_elements(:([4im, 0, 1 + 2im, 2 * 3])) ==
-              (rows = [1, 2, 2], cols = [1, 1, 2], vals = [4im, 1 + 2im, 6]) broken = true
+        @test_broken nonzero_elements(:([4im, 0, 1 + 2im, 2 * 3])) ==
+                     (rows = [1, 2, 2], cols = [1, 1, 2], vals = [4im, 1 + 2im, 6])
     end
 
     @testset "symbols_to_lookup" begin
