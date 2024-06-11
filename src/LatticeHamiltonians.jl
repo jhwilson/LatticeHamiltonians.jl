2fc20b02e3d53beedb700f84219fb426ce84c93e
Zach Raines <dev@zmraines.com>
2024-06-11 16:39:37 -0500

    fix: mul! should be a method of LinearAlgebra.mul!

--- src/LatticeHamiltonians.jl
+++ src/LatticeHamiltonians.jl
@@ -4,13 +4,13 @@ Functions and structures for constructing and applying Hamiltonians on lattice s
 module LatticeHamiltonians
 
 import StaticArrays: SVector, SMatrix, MVector
-import LinearAlgebra: I
+import LinearAlgebra: I, mul!
 import MacroTools
 import MacroTools: isexpr
 import SparseArrays: sparse, dropzeros!
 import Base: *, size, length, eltype, adjoint
 
-export LatticeHamiltonian, mul!, *, sparse, size, length, eltype, adjoint
+export LatticeHamiltonian, *, sparse, size, length, eltype, adjoint
 export @lattice_hamiltonian
 
 include("lattice_hamiltonian.jl")
