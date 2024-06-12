25a50ee8a7f0f4207de273ea3135552119ed0dd2
Zach Raines <dev@zmraines.com>
2024-06-12 09:49:37 -0500

    Macro cleanup (#40)
    * docs: better document behavior of `build_lattice_operator`
    
    * refactor: split main code into multiple files
    
    Code has been split out of the main file into:
    
    - `src/dsl.jl` for the DSL parsing code
    - `src/lattice_hamiltonian.jl` for the construction of and operations on lattice Hamiltonians
    
    * refactor: make imports more specific
    
    * docs: fix minor typo
    
    * refactor: variable names in macros and generated code
    
    This is an attempt to make it clearer when the loop building functions
    are using variables defined in their enclosing scope.
    
    * refactor: split loop generation code into smaller functgions
    
    * docs: add comment clarifying how output is zeroed
    
    * refactor: rename V -> h in hopping function
    
    * refactor: remove unused arguments in make_apply and produced function
    
    * doc: update comment on functions to reference
    
    * fix: provide correct dimension when on-site potential is a function
    
    * doc: add docstrings for loop variables
    
    * refactor: simplify and document parsing of dsl
    
    * refactor: loop_periodic code generation
    
    this removes some redundancy and attempts to
    make the code more readable. Behavior should be the same
    
    * doc: add an example to loop_periodic docs
    
    * refactor: rename loop_periodic functions
    
    This is an attempt to give more descriptive names to
    the core code generation functions.
    
    refactor: remove site_var_prefix argument to codegen functions
    
    It's simplest to just decided on a naming scheme, so for now lets
    name the site indices n1, n2, n3, ...
    
    * fix: mul! should be a method of LinearAlgebra.mul!
    
    * refactor: move linear algebra functions to separate file
    
    This makes clear what code is implementing methods of external linear
    algebra functions.
    Additionally, these methods are now referred to by a qualified scope.
    
    * test: move test dependencies into test Project.toml
    
    * refactor: remove redundant return statements

--- src/build_lattice_operator.jl
+++ src/build_lattice_operator.jl
@@ -1,3 +1,62 @@
+# VARIABLE NAMES
+# ----
+"""
+    site_loop_var(j)
+
+Generate a symbol for the loop variable in the generated code.
+`j` indexes the lattice dimension.
+For example, in a 2D lattice, we might generate `n1`, `n2` for the loop variables.
+
+See also: `site_vector_var`
+"""
+site_loop_var(j) = Symbol("n$j")
+
+"""
+    site_vector_var(dim)
+
+Generate a vector of symbols for the lattice site in the generated code.
+`dim` is the total lattice dimension.
+
+For example, for a 2D lattice,
+
+```jldoctest
+julia> using LatticeHamiltonians: site_var_vector
+
+julia> site_var_vector(2)
+2-element Vector{Symbol}:
+ :n1
+ :n2
+```
+"""
+site_var_vector(dim) = [site_loop_var(j) for j = 1:dim]
+
+"""
+    dim_span_var(j)
+
+Generate a symbol for the span of the lattice dimension in the generated code.
+This describes how much the linear index changes when traversing all sites in this dimension.
+For example, in a 2D lattice, we might generate `Lprod1`, `Lprod2` for the span of the lattice dimensions.
+Suppose that there are `d=3` orbitals on-site, and the lattice dimensions are L1=2 and L2=3.
+Then the linear index changes by `d * L1` when traversing all sites in the first dimension,
+and by `d * L1 * L2` when traversing all sites in the second dimension, so the spans are `6` and `18` respectively.
+"""
+dim_span_var(j) = Symbol("Lprod$j")
+# ----
+
+"""
+    matrix_element(h::Function; site)
+    matrix_element(h::ComplexF64; site)
+    matrix_element(h; site)
+
+Generate an expression for the orbital matrix element `h` at a given lattice site.
+If `h` is a function, it is evaluated at the given site, i.e., `h(n1, n2, n3...)`.
+If `h` is a number, it is returned as a constant.
+If `h` is a symbol, it is looked up from the `params` dictionary.
+"""
+matrix_element(h::Function; site) = :($(h$(site...)))
+matrix_element(h::ComplexF64; site) = h == zero(ComplexF64) ? :() : h
+matrix_element(h; site) = h
+
 """
     make_diag_expr(Vs; s = :n)
 
@@ -5,77 +64,70 @@ Constructs the diagonal part of the Hamiltonian matrix
 in an efficient way by using metaprogramming features of Julia.
 It takes the on-site potentials `Vs` and generates a corresponding `Expr` block,
 which, when evaluated, would perform the operations of the diagonal part of the Hamiltonian.
+
+If `sparse` is set to `true`, the function generates an `Expr` block that
+constructs the sparse matrix representation of the diagonal part of the Hamiltonian.
 """
-function make_diag_expr(Vs; s = :n, sparse = false)
-    expr_array = Vector{Expr}(undef, length(Vs) + 1)
-    for i in eachindex(Vs)
-        V = Vs[i]
-        if sparse
-            expr_array[i] = quote
-                ivals[idx] = ind1 + $i
-                jvals[idx] = ind1 + $i
-                hvals[idx] = $V
-                idx += 1
-            end
-        else
-            if typeof(V) <: ComplexF64
-                expr_array[i] = if (V != zero(ComplexF64))
-                    :(ψout[ind1+$i] = $V * ψin[ind1+$i])
-                else
-                    :(ψout[ind1+$i] = zero(ComplexF64))
-                end
-            elseif typeof(V) <: Symbol
-                expr_array[i] = :(ψout[ind1+$i] = $V * ψin[ind1+$i])
-            elseif typeof(V) <: Function
-                ss = [Symbol("$(s)$m") for m = 1:length(Vs)]
-                expr_array[i] = :(ψout[ind1+$i] = $V($(ss...)) * ψin[ind1+$i])
-            elseif typeof(V) <: Expr
-                expr_array[i] = :(ψout[ind1+$i] = $(V) * ψin[ind1+$i])
-            end
-        end
+function make_diag_expr(Vs, dim; sparse = false)
+    is = eachindex(Vs)
+    expr_array = if sparse
+        @. sparse_diag_expr(Vs, is)
+    else
+        @. diag_expr(Vs, is; site = site_var_vector(dim))
     end
-    expr_array[end] = :(ind1 += d)
-    return Expr(:block, expr_array...)
+
+    push!(expr_array, :(i_out += d))
+    Expr(:block, expr_array...)
+end
+
+sparse_diag_expr(V, i::Int) = sparse_hop_expr(V, :(i_out + $i), :(i_out + $i))
+
+function diag_expr(V, i::Int; site)
+    m_el = matrix_element(V; site = site)
+    if m_el == :()
+        :(ψout[i_out+$i] = zero(ComplexF64))
+    else
+        :(ψout[i_out+$i] = $m_el * ψin[i_out+$i])
+    end
 end
 
 """
-    make_hop_expr(is, js, Vs, dim; s = :n)
+    make_hop_expr(is, js, hs, dim; s = :n, sparse=false)
 
 Constructs the off-diagonal part of the Hamiltonian matrix
 by generating an `Expr` block, which performs the operations of the off-diagonal part of the Hamiltonian.
+
+If `sparse` is set to `true`, the function generates an `Expr` block that
+constructs the sparse matrix representation of the off-diagonal part of the Hamiltonian.
 """
-function make_hop_expr(is, js, Vs, dim; s = :n, sparse = false)
-    expr_array = Vector{Expr}(undef, length(is) + 1)
-    for idx in eachindex(is)
-        i = is[idx]
-        j = js[idx]
-        V = Vs[idx]
-        if sparse
-            expr_array[idx] = quote
-                ivals[idx] = ind2 + $i
-                jvals[idx] = ind1 + $j
-                hvals[idx] = $V
-                idx += 1
-            end
-        else
-            if typeof(V) <: ComplexF64
-                expr_array[idx] = if (V != zero(ComplexF64))
-                    :(ψout[ind1+$j] += $V * ψin[ind2+$i])
-                else
-                    :(zero(ComplexF64))
-                end
-            elseif typeof(V) <: Symbol
-                expr_array[idx] = :(ψout[ind1+$j] += $V * ψin[ind2+$i])
-            elseif typeof(V) <: Function
-                ss = [Symbol("$(s)$m") for m = 1:dim]
-                expr_array[idx] = :(ψout[ind1+$j] += $V($(ss...)) * ψin[ind2+$i])
-            elseif typeof(V) <: Expr
-                expr_array[idx] = :(ψout[ind1+$j] += $(V) * ψin[ind2+$i])
-            end
-        end
+function make_hop_expr(is, js, hs, dim; sparse = false)
+    expr_array = if sparse
+        @. sparse_hop_expr(hs, is, js)
+    else
+        @. hop_expr(hs, is, js; site = site_var_vector(dim))
     end
-    expr_array[end] = :(ind1 += d; ind2 += d)
-    return Expr(:block, expr_array...)
+
+    push!(expr_array, :(i_out += d; i_in += d))
+    Expr(:block, expr_array...)
+end
+
+sparse_hop_expr(V, i::Int, j::Int) = sparse_hop_expr(V, :(i_in + $i), :(i_out + $j))
+function sparse_hop_expr(V, in_idx::Expr, out_idx::Expr)
+    quote
+        ivals[idx] = $in_idx
+        jvals[idx] = $out_idx
+        hvals[idx] = $V
+        idx += 1
+    end
+end
+
+function hop_expr(V, i::Int, j::Int; site)
+    mel = matrix_element(V; site = site)
+    if mel == :()
+        mel
+    else
+        :(ψout[i_out+$j] += $mel * ψin[i_in+$i])
+    end
 end
 
 """
@@ -88,102 +140,181 @@ site_expr(hops) = site_expr(hops, 1)
 
 function site_expr(hops, j)
     if j == length(hops)
-        t = hops[j] ≥ 0 ? :($(hops[j])) : :(L[$j] - $(-hops[j]))
-        return t
+        return hops[j] ≥ 0 ? :($(hops[j])) : :(L[$j] - $(-hops[j]))
     end
-    t = if hops[j] ≥ 0
+    if hops[j] ≥ 0
         :($(hops[j]) + $(site_expr(hops, j + 1)) * L[$j])
     else
         :(L[$j] * (1 + $(site_expr(hops, j + 1))) - $(-hops[j]))
     end
-    return t
 end
 
 """
-    loop_periodic(hops, ex; s = :n)
-    loop_periodic_diag(dim, d, ex; s = :n)
-    loop_periodic(s, hop, ex, j)
+    loop_sites(dim, ex)
 
-Generate an `Expr` block for loops iterating over lattice sites
-in a periodic system.
+Recursively generate an `Expr` block which loops over lattice sites
+and inserts the expressions `ex` at each site.
 """
-function loop_periodic(hops, ex; s = :n)
+function loop_sites(dim, ex)
     quote
-        ind1 = 0
-        ind2 = d * $(site_expr(hops))
-        $(loop_periodic(s, hops, ex, length(hops)))
+        i_out = 0
+        $(loop_site_pairs(zeros(Int, dim), ex, dim))
     end
 end
 
-function loop_periodic_diag(dim, d, ex; s = :n)
+"""
+    loop_site_pairs(hops, ex)
+
+Recursively generate an `Expr` block which loops over all pairs of lattice sites
+separated by the lattice vector `hops` and insert the expression `ex` for each pair.
+
+```jldoctest
+julia> using LatticeHamiltonians: loop_periodic
+
+julia> using MacroTools: prettify
+
+julia> prettify(loop_periodic([1 0 1], :(expr)))
+
+quote
+    i_out = 0
+    i_in = d * (1 + (0 + 1 * L[2]) * L[1])
+    for n3 = 1:L[3] - 1
+        for n2 = 1:L[2]
+            for n1 = 1:L[1] - 1
+                expr
+            end
+            i_in -= Lprod1
+            for n1 = L[1] - 0:L[1]
+                expr
+            end
+            i_in += Lprod1
+        end
+    end
+    i_in -= Lprod3
+    for n3 = L[3] - 0:L[3]
+        for n2 = 1:L[2]
+            for n1 = 1:L[1] - 1
+                expr
+            end
+            i_in -= Lprod1
+            for n1 = L[1] - 0:L[1]
+                expr
+            end
+            i_in += Lprod1
+        end
+    end
+    i_in += Lprod3
+end
+```
+"""
+function loop_site_pairs(hops, ex)
     quote
-        ind1 = 0
-        $(loop_periodic(s, zeros(Int, dim), ex, dim))
+        # initialize the indices of the output and input vectors 
+        i_out = 0
+        i_in = d * $(site_expr(hops))
+        $(loop_site_pairs(hops, ex, length(hops)))
     end
 end
 
-function loop_periodic(s, hop, ex, j)
-    if j == 0
+"""
+    loop_site_pairs(hops, ex, axis)
+
+The main method of `loop_site_pairs` works by recursively calling in to this method.
+Each call to this method generates a block of for loops for a single lattice dimension,
+and then calls the next dimension recursively, decreasing `axis` by 1.
+When we have reached the last dimension, we insert the expression `ex` at each site.
+"""
+function loop_site_pairs(hops, ex, axis)
+    # axis == 1 is the slowest changing lattice dimension
+    # axis == 0 corresponds orbital hoppings
+    # which should already be handled in the supplied expression
+    # so we simply return it
+    if axis == 0
         return ex
     end
-    m::Int = hop[j]
-    sj = Symbol("$(s)$j")
-    Lprodj = Symbol("Lprod$j")
-    if m < 0
-        expr = quote
-            for $sj = 1:$(-m)
-                $(loop_periodic(s, hop, ex, j - 1))
-            end
-            ind2 -= $Lprodj
-            for $sj = $(-m + 1):L[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
-            end
-            ind2 += $Lprodj
-        end
-    elseif m > 0
-        expr = quote
-            for $sj = 1:(L[$j]-$m)
-                $(loop_periodic(s, hop, ex, j - 1))
-            end
-            ind2 -= $Lprodj
-            for $sj = (L[$j]-$(m - 1)):L[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
-            end
-            ind2 += $Lprodj
-        end
-    else
-        expr = quote
-            for $sj = 1:L[$j]
-                $(loop_periodic(s, hop, ex, j - 1))
+
+    # the distance to hop in this dimension
+    hop_range::Int = hops[axis]
+
+    loop_var = site_loop_var(axis)
+    if iszero(hop_range)
+        # this is an on-site hopping in this dimension
+        # just loop over all sites
+        return quote
+            for $loop_var = 1:L[$axis]
+                $(loop_site_pairs(hops, ex, axis - 1))
             end
         end
+    end
+
+    index_before_wrap = hop_range > 0 ? :(L[$axis] - $hop_range) : -hop_range
+    index_after_wrap = hop_range > 0 ? :(L[$axis] - $(hop_range - 1)) : (-hop_range + 1)
+    dim_span = dim_span_var(axis)
+
+    quote
+        for $loop_var = 1:$index_before_wrap
+            $(loop_site_pairs(hops, ex, axis - 1))
+        end
+        i_in -= $dim_span
+        for $loop_var = $index_after_wrap:L[$axis]
+            $(loop_site_pairs(hops, ex, axis - 1))
+        end
+        i_in += $dim_span
     end
 end
 
 """
-    ham_expr(V, T, dim)
+    ham_expr(V, T, dim; sparse=false)
 
 Combine the expressions for the diagonal and hopping terms to generate
 a block of expressions that applies the full Hamiltonian.
+
+The behavior of the function can be controlled by the `sparse` keyword argument:
+
+  - when sparse is `false` (default), the function explicitly performs a matrix-vector multiplication
+    on a vector ψin (which is assumed to be defined in the enclosing scope) and stores the results in ψout.
+
+  - when sparse is `true`, the function generates a sparse matrix representation of the Hamiltonian
+    which can be used to instantiate a sparse matrix.
+
+When implementing matrix-vector multiplication directly, the incoming and outgoing vectors are flattened into 1D arrays.
+This effectively corresponds to a Kroneker product of the lattice dimensions.
+Because Julia uses column-major ordering, the indices of the multi-dimensional array are ordered fastest to slowest changing: [o, i_1, i_2, ..., i_n] where o is the oribital index and i_1, i_2, ..., i_n are the lattice indices in dimensions 1, 2, ..., n.
 """
 function ham_expr(V, T, dim; sparse = false)
-    expr_Lprods = Expr(
+    # since we are flattening the multi-dimensional arrays into 1D arrays,
+    # we need to keep track of the stride of the lattice dimensions
+    # for each lattice dimension
+
+    # the orbital index changes the fastest
+    # then the lattice indices
+    # here we track the span of each lattice dimension
+    # that is how much the linear index when traverse all sites in this dimensions
+    # this is used to implement periodic boundary conditions
+    # where we need to go back to the start of the dimension when we reach the end
+    expr_spans = Expr(
         :block,
         [
-            [:(Lprod1 = d * L[1])]
-            [:($(Symbol("Lprod$i")) = $(Symbol("Lprod$(i-1)")) * L[$i]) for i = 2:dim]
+            [:($(dim_span_var(1)) = d * L[1])]
+            [:($(dim_span_var(i)) = $(dim_span_var(i - 1)) * L[$i]) for i = 2:dim]
         ]...,
     )
-    expr_V = make_diag_expr(V; sparse = sparse)
-    expr_diag = loop_periodic_diag(dim, length(V), expr_V)
+    # evaluate the diagonal elements of the Hamiltonian
+    # NOTE: it is very important that the diagonal code is evaluated first
+    # as it is responsible for zeroing out the output vector
+    expr_V = make_diag_expr(V, dim; sparse = sparse)
+    expr_diag = loop_sites(dim, expr_V)
+
+    # evaluate the off-diagonal elements
     expr_hops = Vector{Expr}(undef, length(T))
     idx = 1
     for (hops, (is, js, hs)) in T
         expr_T = make_hop_expr(is, js, hs, dim; sparse = sparse)
-        expr_hops[idx] = loop_periodic(hops, expr_T)
+        expr_hops[idx] = loop_site_pairs(hops, expr_T)
         idx += 1
     end
-    return Expr(:block, expr_Lprods, expr_diag, expr_hops...)
+
+    Expr(:block, expr_spans, expr_diag, expr_hops...)
 end
 
 """
@@ -192,19 +323,44 @@ end
 Generate an `Expr` that defines a function to apply the Hamiltonian
 given parameters for the potential and hopping terms (V and T).
 """
-function make_apply(params, V, T, dim)
-    # ps = [:($k::typeof($v)) for (k, v) in params]
-    # println(ps)
-    return quote
+function make_apply(V, T, dim)
+    quote
         function (
             ψout::AbstractArray,
             ψin::AbstractArray,
             d::Int,
-            dim::Int,
             L::MVector{$dim,Int64},
             params::Dict{Symbol,ComplexF64},
         )
+            # The arguments of the function are used implicitly
+            # in the generated expressions
+            #
+            # see e.g., diag_expr, hop_expr
             $(ham_expr(V, T, dim))
         end
     end
+end
+
+function build_sparse(H::LatticeHamiltonian, V, T)
+    nz = bound_nonzero(prod(H.L), H.d, T)
+    dim = length(H.L)
+    eval(
+        quote
+            import SparseArrays
+            function SparseArrays.sparse(H::$(typeof(H)))
+                ivals = Array{Int64}(undef, $nz)
+                jvals = Array{Int64}(undef, $nz)
+                hvals = Array{ComplexF64}(undef, $nz)
+                idx = 1
+                params = H.params
+                d = H.d
+                L = H.L
+                $(ham_expr(V, T, dim; sparse = true))
+                idx -= 1
+                return dropzeros!(
+                    sparse(ivals[1:idx], jvals[1:idx], hvals[1:idx], size(H)...),
+                )
+            end
+        end,
+    )
 end
---
+++ src/lattice_hamiltonian.jl
@@ -0,0 +1,96 @@
+import LinearAlgebra: I
+
+"""
+    LatticeHamiltonian{real_dim,lattice_dim,F}
+
+Stores information about the Hamiltonian of the system, so that matrix-vector multiplication
+can be performed efficiently.
+
+This object should not be initialized by the user.
+Instead, for construction of the Hamiltonian, see the `@lattice_hamiltonian` macro.
+
+# Fields
+
+  - `A::SMatrix{real_dim,lattice_dim,Float64}`: Matrix whose columns are the primitive lattice vectors
+  - `B::SMatrix{real_dim,lattice_dim,Float64}`: Matrix whose columns are the reciprocal lattice vectors
+  - `d::Int`: Number of orbitals per site
+  - `L::MVector{lattice_dim,Int}`: System size
+  - `params::Dict{Symbol,ComplexF64}`: Parameters for potential and hopping functions
+  - `r::Vector{SVector{real_dim,Float64}}`: Real-space coordinates of orbitals within a unit cell
+  - `apply!::F`: Function that applies the Hamiltonian to an input wavefunction
+"""
+struct LatticeHamiltonian{real_dim,lattice_dim,F}
+    A::SMatrix{real_dim,lattice_dim,Float64}
+    B::SMatrix{real_dim,lattice_dim,Float64}
+    d::Int
+    L::MVector{lattice_dim,Int}
+    params::Dict{Symbol,ComplexF64}
+    r::Vector{SVector{real_dim,Float64}}
+    apply!::F
+end
+
+"""
+    lattice_hamiltonian(input)
+
+Define a Hamiltonian using a convenient mini domain specific language (described below).
+The user can provide an expression that defines the parameters for the potential and hopping terms (params, V, T, L).
+The macro generates code that constructs a `LatticeHamiltonian` object and a function to apply the Hamiltonian.
+
+# Domain Specific Language
+
+`lattice_hamiltonian` uses a simple domain specific language to define the Hamiltonian.
+It expects the following types of expressions, supplied in any order.
+
+  - **REQUIRED** `L = [L1, L2, ...]`: A vector of integers, of length `lattice_dim` that define the size of the system.
+    The length of this vector defines the dimensionality of the system.
+    And each entry is the the number of unit cells in that direction.
+  - **REQUIRED** `V = [V1, V2, ...]`: A vector of numbers of length `d`, that define the on-site potential
+    for each orbital within a unit cell.
+  - Expressions of the orm `(δ1, δ2, ...) -> T`, with `δi` integers and `T` a \\(d\\times d\\) matrix:
+    The hopping matrix for to hop by `(δ1, δ2, ...)` unit cells.
+
+# Examples
+
+The following creates a Su-Schrieffer-Heeger (SSH) Hamiltonian on a 1D lattice,
+with 10 unit cells.
+
+    H_ssh = @lattice_hamiltonian begin
+        L = [10]
+        V = [0.0, 0.0]
+        (0) -> [0 t1
+                t1 0]
+        (1) -> [0 t2
+                0 0]
+        (-1) -> [0 0
+                t2 0]
+        t1 = 1.0
+        t2 = 2.0
+    end
+"""
+macro lattice_hamiltonian(input)
+    parsed = parse_lattice_dsl(input)
+
+    # Real space structure
+    A = SMatrix{parsed.dim,parsed.dim,Float64}(I) #TODO
+    B = SMatrix{parsed.dim,parsed.dim,Float64}(I * 2 * pi) #TODO
+    r = fill(SVector{parsed.dim}(zeros(Float64, parsed.dim)), parsed.d) #TODO
+
+    apply = eval(make_apply(parsed.V, parsed.T, parsed.dim))
+    H = LatticeHamiltonian(A, B, parsed.d, parsed.L, parsed.params, r, apply)
+
+    build_sparse(H, parsed.V, parsed.T)
+
+    quote
+        $H
+    end
+end
+
+function sitenumber(r, H::LatticeHamiltonian)
+    sitenum = r[H.dim]
+
+    for j = 1:(H.dim-1)
+        sitenum = r[H.dim-j] + sitenum * H.L[H.dim-j]
+    end
+
+    1 + sitenum
+end
